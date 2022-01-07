86abbb8912f50894fc4d30d17dc7886f46f413b1
Edgar <ecarrillo046@gmail.com>
2022-01-06 23:15:32 -0800

    feat: create useTextAnimate and TextAnimateLi

---
+++ src/assets/Animate.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const TextAnimateLi = styled.li`
+  color: var(--clr-gainsboro);
+  transition: color 0.2s;
+
+  ${({ content, index }) => {
+    if (content[index].isActive === false) {
+      return `
+      color: var(--clr-gainsboro-light);  
+    `;
+    }
+  }}
+`;
+
+const useTextAnimate = (data) => {
+  const [content, setContent] = useState(data);
+
+  const setOneActive = (name) => {
+    let contentCopy = [...content];
+
+    contentCopy.forEach((item, index, arr) => {
+      if (arr[index].name !== name) {
+        arr[index].isActive = false;
+      }
+    });
+
+    setContent(contentCopy);
+  };
+
+  const setAllActive = () => {
+    let contentCopy = [...content];
+
+    contentCopy.forEach((item, index, arr) => {
+      arr[index].isActive = true;
+    });
+
+    setContent(contentCopy);
+  };
+
+  return { setOneActive, setAllActive, content };
+};
+
+export { useTextAnimate, TextAnimateLi };
