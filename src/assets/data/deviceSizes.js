f227031bbb0b5737107df6ee5010fb9dbfc4a87d
Edgar <ecarrillo046@gmail.com>
2022-01-06 21:42:18 -0800

    feat: add 3 new devices

--- src/assets/data/deviceSizes.js
+++ src/assets/data/deviceSizes.js
@@ -10,10 +10,13 @@ const size = {
 
 const device = {
   mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
   mobileL: `(max-width: ${size.mobileL})`,
   tablet: `(max-width: ${size.tablet})`,
   laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
   desktop: `(max-width: ${size.desktop})`,
+  desktopL: `(max-width: ${size.desktop})`,
 };
 
 export default device;
