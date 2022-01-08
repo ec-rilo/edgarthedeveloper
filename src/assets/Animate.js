10933eecf2115df44fcc8514501edb1e2cac0b3a
Edgar <ecarrillo046@gmail.com>
2022-01-07 17:09:31 -0800

    feat: create TextCarousel hook, styles, and animations

--- src/assets/Animate.js
+++ src/assets/Animate.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import styled, { keyframes } from 'styled-components';
-
+import { useEffect } from 'react/cjs/react.development';
+import styled, { css, keyframes } from 'styled-components';
+/* Animations */
 const fadeInFromLeft = keyframes`
   0% {
     opacity: 0;
@@ -13,6 +14,96 @@ const fadeInFromLeft = keyframes`
   }
 `;
 
+/* Text Carousel Hook, Styles, and animations */
+const carouselAnimEntering = keyframes`
+  0% {
+    opacity: 0;
+    left: -100%;
+  }
+
+  100% {
+    opacity: 1;
+    left: 0;
+  }
+`;
+
+const carouselAnimExiting = keyframes`
+  0% {
+    opacity: 1;
+    left: 0;
+  }
+
+  100% {
+    opacity: 0;
+    left: 100%; 
+  }
+`;
+
+const textCarouselStyles = `position: absolute; left: -100%; `;
+const textCarouselLogic = ({ data }) => {
+  // if the current element is active
+  // give it a left of 0
+
+  if (data.isInitial === true) {
+    return `
+      left: -100%;
+      opacity: 0;
+    `;
+  } else if (data.isEntering === true) {
+    return css`
+      animation: ${carouselAnimEntering} 1s forwards;
+    `;
+  } else if (data.isExiting === true) {
+    return css`
+      animation: ${carouselAnimExiting} 1s forwards;
+    `;
+  }
+};
+
+const textCarouselContainerStyles = `position: relative;`;
+
+const useTextCarousel = (data) => {
+  const [content, setContent] = useState(data);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let contentCopy = [...content];
+
+      for (let i = 0; i < contentCopy.length; ++i) {
+        if (contentCopy[i].isEntering === true) {
+          contentCopy[i].isExiting = true;
+          contentCopy[i].isEntering = false;
+
+          if (i === contentCopy.length - 1) {
+            contentCopy[0].isEntering = true;
+            contentCopy[0].isInitial = false;
+          } else {
+            contentCopy[i + 1].isEntering = true;
+            contentCopy[i + 1].isInitial = false;
+          }
+
+          for (let j = 0; j < contentCopy.length; ++j) {
+            if (
+              contentCopy[j] !== contentCopy[i] &&
+              contentCopy[j].isExiting === true
+            ) {
+              contentCopy[j].isExiting = false;
+            }
+          }
+          break;
+        }
+      }
+
+      setContent(contentCopy);
+    }, 2500);
+
+    return () => clearInterval(interval);
+  });
+
+  return { content };
+};
+
+/* Text Animate Hook*/
 const TextAnimateImg = styled.img`
   opacity: 1;
   transition: opacity 0.2s;
@@ -69,4 +160,13 @@ const useTextAnimate = (data) => {
   return { setOneActive, setAllActive, content };
 };
 
-export { useTextAnimate, TextAnimateLi, TextAnimateImg, fadeInFromLeft };
+export {
+  useTextAnimate,
+  TextAnimateLi,
+  TextAnimateImg,
+  fadeInFromLeft,
+  textCarouselStyles,
+  useTextCarousel,
+  textCarouselLogic,
+  textCarouselContainerStyles,
+};
