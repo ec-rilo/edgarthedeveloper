9a58795340c6f87e3dc7388fbcfe50291b4848d5
Edgar <ecarrillo046@gmail.com>
2022-01-07 17:10:21 -0800

    feat: add StyledIntroContainer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import StyledNavContainer from '../Components/NavBar';
 import { Container, SubContainer } from '../Components/Container';
 import { StyledSocialMediaContainer1 } from '../Components/SocialMediaContainers';
+import StyledIntroContainer from '../Components/Intro';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <SubContainer>
           <StyledNavContainer />
           <StyledSocialMediaContainer1 homepage />
+          <StyledIntroContainer homepage />
         </SubContainer>
       </Container>
     </div>
