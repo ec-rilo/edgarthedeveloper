37037304cd966365651f71e6f76e895567d6c491
Edgar <ecarrillo046@gmail.com>
2022-01-10 15:46:38 -0800

    feat: import MainPage in place of path=/

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
+import MainPage from './Pages/MainPage';
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
