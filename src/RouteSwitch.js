c3e7045f75d0f0586eeb28bafeec958054c52c28
Edgar <ecarrillo046@gmail.com>
2022-01-10 15:50:02 -0800

    feat: changed import path

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainPage from './Pages/MainPage';
+import MainPage from './pages/MainPage';
 
 const RouteSwitch = () => {
   return (
