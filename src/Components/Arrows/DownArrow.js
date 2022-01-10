28cb199705c218ba99ead2f3c2ad5a583e5e8ded
Edgar <ecarrillo046@gmail.com>
2022-01-10 13:33:38 -0800

    feat: create downarrow and downarrow container components

---
+++ src/Components/Arrows/DownArrow.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import arrowHeadSrc from './down-arrow-head.png';
+
+const ArrowBody = styled.div`
+  background-color: var(--clr-gainsboro);
+  height: 18px;
+  width: 1px;
+`;
+
+const ArrowHead = styled.img`
+  position: absolute;
+  display: block;
+  width: 15px;
+  height: 15px;
+  object-fit: contain;
+  bottom: -3px;
+`;
+
+const DownArrow = ({ className }) => {
+  return (
+    <div className={className}>
+      <ArrowBody />
+      <ArrowHead src={arrowHeadSrc} alt="" />
+    </div>
+  );
+};
+
+const StyledDownArrow = styled(DownArrow)`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const DownArrowContainer = ({ className }) => {
+  return (
+    <div className={className}>
+      <p>Scroll Down</p>
+      <StyledDownArrow />
+    </div>
+  );
+};
+
+const StyledDownArrowContainer = styled(DownArrowContainer)`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  cursor: pointer;
+  width: 110px;
+  margin: 50px 0;
+`;
+
+export { StyledDownArrowContainer };
