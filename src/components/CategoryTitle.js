3293e5cdc9419bf5ffdc3fa750756aab4011c23d
Edgar <ecarrillo046@gmail.com>
2022-01-11 12:42:59 -0800

    feat: create categorytitle component

---
+++ src/components/CategoryTitle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const CategoryTitle = styled.p`
+  color: var(--clr-amber);
+  font-size: 1rem;
+  font-family: var(--fnt-regular);
+`;
+
+export default CategoryTitle;
