d6b9619a470463c8c38083f013d31dea510acd90
Edgar <ecarrillo046@gmail.com>
2022-01-11 13:37:50 -0800

    feat: add overlay to img on hover

--- src/components/ProjectComponents/ProjectImg.js
+++ src/components/ProjectComponents/ProjectImg.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import device from '../../assets/data/deviceSizes';
+import StyledRightArrow from '../Arrows/RightArrow';
 
 const Img = styled.img`
   object-fit: contain;
@@ -7,17 +9,97 @@ const Img = styled.img`
   width: 100%;
 `;
 
+const DarkOverlay = styled.div`
+  position: absolute;
+  background-color: var(--clr-onyx);
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: opacity 250ms ease-in-out;
+
+  ${(props) => {
+    if (props.isActive === true) {
+      return `opacity: 1;`;
+    }
+  }}
+`;
+
+const CenteredText = styled.p`
+  color: var(--clr-gainsboro);
+  font-family: var(--fnt-regular);
+  font-size: 1.5rem;
+  text-transform: uppercase;
+`;
+
+const CenteredContainer = styled.div`
+  position: absolute;
+  display: flex;
+  gap: 30px;
+  flex-direction: row;
+  opacity: 0;
+  transform: translateY(-400%);
+  transition: opacity 250ms ease-in-out, transform 250ms ease-in-out;
+
+  ${(props) => {
+    if (props.isActive === true) {
+      return `
+      opacity: 1;
+      transform: translateY(0);
+      `;
+    }
+  }}
+`;
+
+const SelectedImgOverlay = ({ className }) => {
+  const [isActive, setActive] = useState(false);
+
+  const toggleActive = () => {
+    isActive === true ? setActive(false) : setActive(true);
+  };
+
+  return (
+    <div
+      className={className}
+      onMouseEnter={() => toggleActive()}
+      onMouseLeave={() => toggleActive()}
+    >
+      <DarkOverlay isActive={isActive} />
+      <CenteredContainer isActive={isActive}>
+        <CenteredText>View Project</CenteredText>
+        <StyledRightArrow />
+      </CenteredContainer>
+    </div>
+  );
+};
+
+const StyledSelectedImgOverlay = styled(SelectedImgOverlay)`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
 const ProjectImg = ({ className, src, alt }) => {
+  // when mouse hovers over the image
+
+  // a dark overlay appears over the image with View Project on the overlay
+
   return (
     <div className={className}>
+      <StyledSelectedImgOverlay />
       <Img src={src} alt={alt} />
     </div>
   );
 };
 
 const StyledProjectImg = styled(ProjectImg)`
+  position: relative;
   background-color: var(--clr-gainsboro);
   height: 400px;
+  cursor: pointer;
+
   @media ${device.laptop} {
     height: 250px;
   }
