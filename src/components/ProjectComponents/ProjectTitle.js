02bcabfb45c7f5bba877b2c50e7533ee70eb6004
Edgar <ecarrillo046@gmail.com>
2022-01-11 12:41:23 -0800

    feat: create projecttitle component

---
+++ src/components/ProjectComponents/ProjectTitle.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+const ProjectTitle = styled.h3`
+  color: var(--clr-gainsboro);
+  font-family: var(--fnt-bld);
+  font-size: 2rem;
+`;
+export default ProjectTitle;
