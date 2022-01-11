c70f0deef1883ee1822591b683518d14c3b7254c
Edgar <ecarrillo046@gmail.com>
2022-01-10 17:37:14 -0800

    feat: add font family's to styleword and styledparagraph components

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 const StyledWord = styled.span`
+  font-family: var(--fnt-regular);
   color: var(--clr-amber);
 `;
 
 const StyledParagraph = styled.p`
+  font-family: var(--fnt-regular);
   color: var(--clr-gainsboro);
   font-size: 1.1rem;
   max-width: 600px;
