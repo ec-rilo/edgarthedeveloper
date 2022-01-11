ad1035e19d6017e36df2b5ac1dfa67fed333bac5
Edgar <ecarrillo046@gmail.com>
2022-01-10 17:35:39 -0800

    feat: add content

--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -1,11 +1,36 @@
-const SkillsPage = () => {
+import styled from 'styled-components';
+import { StyledArticle } from '../components/Article';
+import { SubContainer } from '../components/Container';
+import PageTitle from '../components/PageTitle';
+import { StyledParagraph } from '../components/Paragraph';
+import StyledSkillsBanner from '../components/SkillsBanner';
+
+const SkillsPage = ({ className }) => {
   return (
-    <article>
-      <section></section>
-      <section></section>
-      <section></section>
-    </article>
+    <SubContainer>
+      <article className={className}>
+        <PageTitle>My Skills</PageTitle>
+        <StyledArticle>
+          <StyledParagraph>
+            I’ve taken courses that had an emphasis on understanding web
+            development technologies as well as the basics of computer science
+            concepts. After having pair programmed with many different people
+            and applied the web development concepts into many projects you can
+            now see my skills reflected on all new projects I’ve created.
+          </StyledParagraph>
+          <StyledParagraph>
+            My favorite skill on this journey has been the endlesss learning.
+            Have a job available with tech not shown below? I’m ready.
+          </StyledParagraph>
+        </StyledArticle>
+        <StyledSkillsBanner />
+      </article>
+    </SubContainer>
   );
 };
 
-export default SkillsPage;
+const StyledSkillsPage = styled(SkillsPage)`
+  padding: 12% 0%;
+`;
+
+export default StyledSkillsPage;
